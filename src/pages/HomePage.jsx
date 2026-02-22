8e0e9dc379a9903869c00cf6bdbeba97981a0b6d
Ziad-Alghawi <ziad97algawy@gmail.com>
2026-02-22 02:56:27 +0100

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,15 @@ import CheckMarkIcon from '../assets/images/icons/checkmark.png';
 import './HomePage.css';
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+  .then((response) =>{
+    return response.json();
+    }).then((data) => {
+      console.log(data);
+
+  });
+
+
   return (
     <>
 
