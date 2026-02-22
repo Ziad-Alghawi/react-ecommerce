5e2f2c9aa7dcc0e9d8ffd9bb64c6d2a0786e9c0b
Ziad-Alghawi <ziad97algawy@gmail.com>
2026-02-22 03:26:46 +0100

    Lift cart up and share it beweeb pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
@@ -6,19 +8,28 @@ import { TrackingPage } from './pages/TrackingPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import './App.css'
 
+
 function App() {
+  const [cart, setCart] = useState([]);
   
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data);
+      })
+  }, []);
+
 
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
-      <Route path="tracking"  element={<TrackingPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
 
-    )
+  )
 }
 
 export default App
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products';
 import CheckMarkIcon from '../assets/images/icons/checkmark.png';
 import './HomePage.css';
 
-export function HomePage() {
+export function HomePage({cart}) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+  
 
   useEffect(() => {
     axios.get('/api/products')
@@ -15,10 +14,7 @@ export function HomePage() {
         setProducts(response.data);
       });
 
-    axios.get('/api/cart-items')
-      .then((response) => {
-        setCart(response.data);
-      })
+    
   }, []);
 
   return (
