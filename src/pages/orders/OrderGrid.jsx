31414f840c1bc29f41ec9c389674fa7047372c0a
Ziad-Alghawi <ziad97algawy@gmail.com>
2026-02-23 06:10:29 +0100

    Separate OrdersPage into components like OrdersGrid and OrderHeader and OrderDetailsGrid

---
+++ src/pages/orders/OrderGrid.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import { OrderHeader } from './OrderHeader';
+import { OrderDetailsGrid } from './OrderDetailsGrid';
+
+
+export function OrderGrid({ orders }) {
+  return (
+
+    <div className="orders-grid">
+      {orders.map((order) => {
+        return (
+
+          <div key={order.id} className="order-container">
+
+            <OrderHeader order={order} />
+
+            <OrderDetailsGrid order={order} />
+          </div>
+        );
+      })}
+    </div>
+
+
+  );
+}
