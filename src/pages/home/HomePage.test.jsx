f886d5a86c87f04ca8eaed055bd35ab41e32eeb4
Ziad-Alghawi <ziad97algawy@gmail.com>
2026-03-01 03:29:06 +0100

    checking add to cart button + update also, in HomePage >> g-h

--- src/pages/home/HomePage.test.jsx
+++ src/pages/home/HomePage.test.jsx
@@ -1,6 +1,7 @@
 import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import { HomePage } from './HomePage';
 
@@ -8,6 +9,7 @@ vi.mock('axios');
 
 describe('HomePage component', () => {
   let loadCart;
+  let user;
 
   axios.get.mockImplementation(async (urlPath) => {
     if (urlPath === '/api/products') {
@@ -41,6 +43,7 @@ describe('HomePage component', () => {
 
   beforeEach(() => {
   loadCart = vi.fn();
+  user = userEvent.setup();
 });
   it('displays the product correctly', async () => {
     render(
@@ -65,4 +68,45 @@ describe('HomePage component', () => {
     ).toBeInTheDocument();
     
   });
+
+  
+// we will test the add to cart functionality in this test case
+  it('adds a product to the cart', async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCart} />
+      </MemoryRouter>
+    );
+
+    const productContainer =await screen.findAllByTestId('product-container');
+    // we will add 2 products to the cart with different quantities
+    const quantitySelector1 = within(productContainer[0]).getByTestId('quantity-select-selector');
+    await user.selectOptions(quantitySelector1, '2');
+// we will click add for first product
+    const addToCartButton1 = within(productContainer[0]).getByTestId('add-to-cart-button');
+    await user.click(addToCartButton1);
+
+//////////////////////// 
+
+    // we will add the second product to the cart with a different quantity
+    const quantitySelector2 = within(productContainer[1]).getByTestId('quantity-select-selector');
+    await user.selectOptions(quantitySelector2, '3');
+    // we will click add for  second product
+    const addToCartButton2 = within(productContainer[1]).getByTestId('add-to-cart-button');
+    await user.click(addToCartButton2);
+
+    // check if the axios.post was called with the correct parameters for both products
+    expect(axios.post).toHaveBeenNthCalledWith(1, '/api/cart-items', {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2
+    });
+
+    expect(axios.post).toHaveBeenNthCalledWith(2, '/api/cart-items', {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 3
+    });
+
+    expect(loadCart).toHaveBeenCalledTimes(2);
+
+  })
 });
